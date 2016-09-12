1599a0513332db737f820f5852273186e3a28f49
giginet <giginet.net@gmail.com>
2016-09-12 16:47:38 +0900

    Add wait for testcase

--- Tests/Phakchi/MockServiceClientTest.swift
+++ Tests/Phakchi/MockServiceClientTest.swift
@@ -23,9 +23,15 @@ class MockServiceClientTestCase: XCTestCase {
         self.client.cleanInteractions()
 
         let expectation = expectationWithDescription("session is closed")
-        self.session.close { _ in
-            expectation.fulfill()
+
+        // workaround to pass test on CI
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            self.session.close { _ in
+                expectation.fulfill()
+            }
         }
+
         waitForExpectationsWithTimeout(5.0, handler: nil)
     }
 
