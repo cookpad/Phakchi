c1fbc303ad783e1194c4555ab8e6b2be842f7a63
Kohki Miki <koki-miki@cookpad.com>
2016-11-22 14:06:22 +0900

    Remove PactEncodable from Interaction

--- Sources/Phakchi/Interaction.swift
+++ Sources/Phakchi/Interaction.swift
@@ -6,8 +6,8 @@ public typealias Query = [String: PactEncodable]
 public typealias Headers = [String: PactEncodable]
 public typealias Body = PactEncodable
 
-struct Interaction: PactEncodable {
-    struct Request: PactEncodable {
+struct Interaction {
+    struct Request {
         let method: HTTPMethod
         let path: PactEncodable
         var query: Query?
@@ -32,7 +32,7 @@ struct Interaction: PactEncodable {
         }
     }
 
-    struct Response: PactEncodable {
+    struct Response {
         let status: Int
         let headers: Headers?
         let body: Body?
